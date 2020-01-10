68685a0cf06e5ed645aa3d92af8e86a4267a09bb
Naman Chikara <singhnamanchikara@gmail.com>
2020-01-10 23:13:51 +0530

    first commit

---
+++ src/test/java/me/naman/todoapp/todoapp/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.naman.todoapp.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
